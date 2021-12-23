e24c1d4c5f15f9169a409fd6f3d77d7752feabf1
David Horak <info@davidhorak.com>
2021-12-23 14:17:45 -0500

    Feature/error handling (#4)
    * add icons
    
    * add notifications

--- src/App.svelte
+++ src/App.svelte
@@ -3,19 +3,47 @@
   import { parse, probe } from '@spaceavocado/librarian'
   import type { ProbeResult, EvaluationResult } from '@spaceavocado/librarian'
   import { pipe } from './common/fp'
+  import { fade } from 'svelte/transition'
 
   import Probe from './components/probe/Probe.svelte'
   import Expression from './components/expression/Expression.svelte'
   import Context from './components/context/Context.svelte'
+  import Snackbar, { Type } from './components/snackbar/Snackbar.svelte'
 
   let context = ''
   let expression = ''
 
   let searchResult: EvaluationResult = false
   let probeResult: ProbeResult | undefined
+  let notifications: {
+    id: symbol
+    type: Type
+    message: string
+  }[] = []
+
+  const removeNotification = (id: symbol) => {
+    notifications = notifications.filter(
+      (notification) => notification.id !== id
+    )
+  }
 
   const search = () => {
-    ;[searchResult, probeResult] = probe(parse(expression)).execute(context)
+    try {
+      ;[searchResult, probeResult] = probe(parse(expression)).execute(context)
+    } catch (e) {
+      const message: string = e.message ?? ''
+      notifications = [
+        ...notifications,
+        {
+          id: Symbol(),
+          type: Type.Error,
+          message: message[0].toUpperCase() + message.substring(1) + '.',
+        },
+      ]
+
+      searchResult = false
+      probeResult = undefined
+    }
   }
 </script>
 
@@ -28,6 +56,18 @@
   footer {
     @include font-size($fs-xs);
     opacity: 0.7;
+  }
+  .notifications {
+    position: absolute;
+    bottom: $vs-1;
+    width: 100%;
+    pointer-events: none;
+    :global(.snackbar) {
+      pointer-events: auto;
+    }
+    :global(.snackbar + .snackbar) {
+      margin-top: $vs-1;
+    }
   }
 </style>
 
@@ -56,7 +96,7 @@
       <div class="keyline--1" />
     </div>
     {#if probeResult}
-      <div class="row">
+      <div class="row" in:fade={{ duration: 300 }}>
         <div class="vs--xs--2" />
         <div class="col--xs--12">
           <Probe probe={probeResult} />
@@ -82,5 +122,15 @@
       search of greatness.
       <div class="vs--xs--2" />
     </footer>
+  </div>
+  <div class="notifications">
+    {#each notifications as notification (notification.id)}
+      <Snackbar
+        type={notification.type}
+        duration={5000}
+        on:close={() => removeNotification(notification.id)}
+        >{notification.message}</Snackbar
+      >
+    {/each}
   </div>
 </main>
